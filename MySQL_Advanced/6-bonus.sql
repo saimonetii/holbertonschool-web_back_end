2e040b8f278a71f423ef970a46bf85553106dca1
saimonetii <samuel.coronado.c.dbb@gmail.com>
2026-02-09 21:46:28 +0000

    Task 6: Add bonus

--- MySQL_Advanced/6-bonus.sql
+++ MySQL_Advanced/6-bonus.sql
@@ -1,3 +1,4 @@
+-- Script that creates a stored procedure AddBonus that adds a new correction for a student.
 DELIMITER $$
 CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
