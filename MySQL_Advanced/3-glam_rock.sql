75d678f364eb24c1f5a30e2cf91695c42e4beb61
saimonetii <samuel.coronado.c.dbb@gmail.com>
2026-02-09 21:34:46 +0000

    Task 3: Old school band 2024

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
--- Script old school band ajustado para pasar el checker (usando 2022)
-SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
+-- Script old school band calculado hasta 2024
+SELECT band_name, (IFNULL(split, 2024) - formed) AS lifespan
     FROM metal_bands
     WHERE style LIKE '%Glam rock%'
     ORDER BY lifespan DESC;
